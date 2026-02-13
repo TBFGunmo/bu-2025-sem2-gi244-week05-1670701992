0e73445321b5ede55cac3fa772700d949c731f1a
TBFGunmo <gunmohome123@gmail.com>
2026-02-13 10:27:14 +0700

    WIP

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    public int attackPoint = 5;
+    public int attackPoint = 50;
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Hit" + other.gameObject.name);
+        var health = other.gameObject.GetComponent<HealthV1>();
+
+        if (health) 
+        {
+            health.TakeDamage(attackPoint);
+        }
 
     }
 }
--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -2,8 +2,46 @@ using UnityEngine;
 
 public class WaveController : MonoBehaviour
 {
+    public Wave currentWave;
+    public Transform[] spawnPoint;
+
+    private int enemySpawned = 0;
+    private float nextSpawnTime = 0;
+
+    public Wave[] waveConfigs;
+
     void Update()
     {
-
+        var t = Time.time;
+        if (t > nextSpawnTime && enemySpawned < currentWave.enemyCount) 
+        {
+            Spawn();
+            enemySpawned++;
+            nextSpawnTime += Time.time + currentWave.spawnInterval;
+        }
     }
+
+    void Spawn()
+    {
+        //animalIndex = Random.Range(0, animalPrefabs.Length);
+        //Vector3 spawnPos = new(
+        //    Random.Range(-spawnRangeX, spawnRangeX),
+        //    transform.position.y,
+        //    transform.position.z
+        //);
+        //Instantiate(
+        //    animalPrefabs[animalIndex],
+        //    spawnPos,
+        //    animalPrefabs[animalIndex].transform.rotation
+        //);
+
+        int enemyIndex = Random.Range(0, currentWave.enemyPrefabs.Length);
+        int spawnPointIndex = Random.Range(0, spawnPoint.Length);
+
+        Instantiate(
+            currentWave.enemyPrefabs[enemyIndex], 
+            spawnPoint[spawnPointIndex].position, 
+            currentWave.enemyPrefabs[enemyIndex].transform.rotation);
+    }
+
 }
